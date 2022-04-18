75ad2fd037b8befcb8e504067eee518ae322f1c6
Shaneumayanga <shaneumayanga445@gmail.com>
2022-04-18 22:59:41 +0530

    Lol

--- genproto.sh
+++ genproto.sh
@@ -1,1 +1,3 @@
 protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative streaming/stream.proto
+
+#python_out
---
+++ genprotopy.sh
@@ -0,0 +1,2 @@
+protoc -I=. --python_out=. --python-grpc_out=. streaming/stream.proto
+
